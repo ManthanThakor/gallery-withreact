6a6598bc6bdaf7f5bc8bd11f3635b5b6ff0732a1
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-22 13:14:53 +0530

    Change 60

--- src/components/Home/Coolsl.jsx
+++ src/components/Home/Coolsl.jsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../../styles/home/Coolsl.css";
 
 function ImageSlider() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const settings = {
     centerMode: true,
-    centerPadding: "60px",
+    centerPadding: "0px",
     slidesToShow: 3,
+    speed: 500,
+    easing: "ease-in-out",
+    arrows: false, // Removed arrow controls
+    dots: true, // Enable dots
+    afterChange: (current) => setCurrentSlide(current), // Update current slide index after each change
+    appendDots: (dots) => (
+      <div>
+        <ul className="custom-dots">{dots}</ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div className="dot-bar-wrapper">
+        <div
+          className="dot-bar"
+          style={{
+            width: `${100 / 6}%`,
+            transform: `translateX(${(currentSlide * 100) / 6}%)`, // Update transform based on current slide
+          }}
+        ></div>
+      </div>
+    ),
     responsive: [
       {
         breakpoint: 768,
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: "40px",
+          centerPadding: "20px",
           slidesToShow: 3,
         },
       },
@@ -24,7 +47,7 @@ function ImageSlider() {
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: "40px",
+          centerPadding: "20px",
           slidesToShow: 1,
         },
       },
@@ -32,12 +55,30 @@ function ImageSlider() {
   };
 
   const images = [
-    "https://images.pond5.com/anime-girl-garden-among-colorful-footage-229627183_iconl.jpeg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4C3Qq_8gVU7soDvwQ7R0wtb9yZjFjtofly_bIeavpPh15Le0fqVEPSplYewYogiJd6kk&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlC9b0SBKtu1jL1nctoWyr77vi-YXPhDEaF5cjy3cqJbejcfsOOiGrw8fCOkovM0xWX3k&usqp=CAU",
-    "https://images.pond5.com/anime-girl-garden-among-colorful-footage-229627183_iconl.jpeg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4C3Qq_8gVU7soDvwQ7R0wtb9yZjFjtofly_bIeavpPh15Le0fqVEPSplYewYogiJd6kk&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlC9b0SBKtu1jL1nctoWyr77vi-YXPhDEaF5cjy3cqJbejcfsOOiGrw8fCOkovM0xWX3k&usqp=CAU",
+    {
+      src: "https://images.pond5.com/anime-girl-garden-among-colorful-footage-229627183_iconl.jpeg",
+      title: "Anime Girl in Garden",
+    },
+    {
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4C3Qq_8gVU7soDvwQ7R0wtb9yZjFjtofly_bIeavpPh15Le0fqVEPSplYewYogiJd6kk&usqp=CAU",
+      title: "Beautiful Landscape",
+    },
+    {
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlC9b0SBKtu1jL1nctoWyr77vi-YXPhDEaF5cjy3cqJbejcfsOOiGrw8fCOkovM0xWX3k&usqp=CAU",
+      title: "Scenic View",
+    },
+    {
+      src: "https://images.pond5.com/anime-girl-garden-among-colorful-footage-229627183_iconl.jpeg",
+      title: "Anime Girl in Garden",
+    },
+    {
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4C3Qq_8gVU7soDvwQ7R0wtb9yZjFjtofly_bIeavpPh15Le0fqVEPSplYewYogiJd6kk&usqp=CAU",
+      title: "Beautiful Landscape",
+    },
+    {
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlC9b0SBKtu1jL1nctoWyr77vi-YXPhDEaF5cjy3cqJbejcfsOOiGrw8fCOkovM0xWX3k&usqp=CAU",
+      title: "Scenic View",
+    },
   ];
 
   return (
@@ -45,7 +86,8 @@ function ImageSlider() {
       <Slider {...settings}>
         {images.map((image, index) => (
           <div key={index} className="slide">
-            <img src={image} alt={`Slide ${index + 1}`} />
+            <img src={image.src} alt={`Slide ${index + 1}`} />
+            <div className="slide-title">{image.title}</div>
           </div>
         ))}
       </Slider>
